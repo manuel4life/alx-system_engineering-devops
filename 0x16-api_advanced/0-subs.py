79cbeebf8b2424960f70f34b997b2f0c67af0ebf
manuel4life <allotey4eva@gmail.com>
2023-02-01 09:22:10 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
+
+"""
+    This function retrieves the number of subscribers of a given subreddit.
+    Parameters:
+    subreddit (str): The subreddit to retrieve the number of subscribers for
+    Returns:
+    int: The number of subscribers of the subreddit. If the subreddit does not
+    exist or there is a problem with the request, returns 0.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0."""
-    headers = {'User-Agent': 'My Reddit API Client'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    This function retrieves the number of subscribers of a given subreddit.
+    Parameters:
+    subreddit (str): The subreddit to retrieve the number of subscribers for
+    Returns:
+    int: The number of subscribers of the subreddit. If the subreddit does not
+    exist or there is a problem with the request, returns 0.
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/\
+          537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+    }
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data.get('data', {}).get('subscribers', 0)
+        subscribers = data["data"]["subscribers"]
         return subscribers
     else:
         return 0
